09e26459276bacb47cee2389cd581a520d01b42d
undrfined <undrfined@gmail.com>
2021-05-17 00:24:41 +0300

    gamer model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Gamer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
@@ -12,14 +13,14 @@ class UserController extends Controller
 {
     public function login(Request $request)
     {
-        $users = DB::select('select * from gamers');
+
         $validator = Validator::make($request->all(), [
             'login' => 'required|email|min:6|max:64|regex:/^\S+@\S+$/',
             'password' => 'required|min:6|max:32|regex:/[a-zA-Z0-9]+/'
         ]);
 
         if ($validator->fails()) {
-            return ['ok' => false, 'messages' => $validator->messages(), $users];
+            return ['ok' => false, 'messages' => $validator->messages()];
         }
 
         return ['ok' => true, "avatar" => "https://i.stack.imgur.com/dr5qp.jpg"];
@@ -39,6 +40,15 @@ class UserController extends Controller
             return ['ok' => false, 'messages' => $validator->messages()];
         }
 
-        return ['ok' => true, "avatar" => "https://i.stack.imgur.com/dr5qp.jpg"];
+        $gamer = Gamer::create([
+            'name' => $request->input("login"),
+            'email' => $request->input("login"),
+            'gender' => $request->input("gender"),
+            'bio' => $request->input("bio"),
+            'skill' => $request->input("skillLevel"),
+            'password' => $request->input("password"),
+        ]);
+
+        return ['ok' => true, "avatar" => "https://i.stack.imgur.com/dr5qp.jpg", $gamer];
     }
 }
